4df6821585221940955524305efbad778d2fe019
cozej4 <ilakozejumanne@gmail.com>
2025-02-21 10:34:25 +0300

    :hammer: Refactored DB constants and fixed some codacy issues

--- opensrp-chw-hps/src/main/java/org/smartregister/chw/hps/util/DBConstants.java
+++ opensrp-chw-hps/src/main/java/org/smartregister/chw/hps/util/DBConstants.java
@@ -4,31 +4,93 @@ public class DBConstants {
 
     public final class KEY {
         public static final String FIRST_NAME = "first_name";
+
         public static final String MIDDLE_NAME = "middle_name";
+
         public static final String LAST_NAME = "last_name";
+
         public static final String BASE_ENTITY_ID = "base_entity_id";
+
         public static final String FAMILY_BASE_ENTITY_ID = "family_base_entity_id";
+
         public static final String DOB = "dob";
+
         public static final String DOD = "dod";
+
         public static final String UNIQUE_ID = "unique_id";
+
         public static final String MARITAL_STATUS = "marital_status";
+
         public static final String LAST_INTERACTED_WITH = "last_interacted_with";
+
         public static final String VILLAGE_TOWN = "village_town";
+
         public static final String DATE_REMOVED = "date_removed";
+
         public static final String GENDER = "gender";
+
         public static final String RELATIONALID = "relationalid";
+
         public static final String DETAILS = "details";
+
         public static final String IS_CLOSED = "is_closed";
+
         public static final String IS_ANC_CLOSED = "is_anc_closed";
+
         public static final String IS_PNC_CLOSED = "is_pnc_closed";
+
         public static final String RELATIONAL_ID = "relational_id";
+
         public static final String FAMILY_HEAD = "family_head";
+
         public static final String PRIMARY_CARE_GIVER = "primary_caregiver";
+
         public static final String LAST_HOME_VISIT = "last_home_visit";
+
         public static final String VISIT_NOT_DONE = "visit_not_done";
+
         public static final String FAMILY_NAME = "family_name";
+
         public static final String PHONE_NUMBER = "phone_number";
+
         public static final String CHILD = "ec_child";
 
+        public static final String DATE_OF_GATHERING = "date_of_gathering";
+
+        public static final String THE_METHOD_OF_EDUCATION_AND_AWARENESS_USED = "the_method_of_education_and_awareness_used";
+
+        public static final String AREA_WHERE_MOBILIZATION_TAKES_PLACE = "area_where_mobilization_takes_place";
+
+        public static final String NUMBER_OF_FEMALES_WHO_ATTENDED = "number_of_females_who_attended";
+
+        public static final String NUMBER_OF_MALES_WHO_ATTENDED = "number_of_males_who_attended";
+
+        public static final String WAS_EDUCATION_PROVIDED = "was_education_provided";
+
+        public static final String EDUCATION_PROVIDED = "education_provided";
+
+        public static final String INFORMATION_EDUCATION_AND_COMMUNICATION_MATERIAL = "information_education_and_communication_material";
+
+        public static final String BROCHURE_MATERIALS = "brochure_materials";
+
+        public static final String NUMBER_OF_BROCHURES_PROVIDED = "number_of_brochures_provided";
+
+        public static final String POSTER_MATERIALS = "poster_materials";
+
+        public static final String NUMBER_OF_POSTERS_PROVIDED = "number_of_posters_provided";
+
+        public static final String LEAFLET_MATERIALS = "leaflet_materials";
+
+        public static final String NUMBER_OF_LEAFLET_PROVIDED = "number_of_leaflet_provided";
+
+        public static final String OTHER_IEC_MATERIALS = "other_iec_materials";
+
+        public static final String NUMBER_OF_OTHER_IEC_PROVIDED = "number_of_other_iec_provided";
+
+        public static final String SEX = "sex";
+
+        public static final String CAUSE_OF_DEATH = "cause_of_death";
+
+        public static final String CAUSE_OF_DEATH_SPECIFY = "cause_of_death_specify";
     }
 }
