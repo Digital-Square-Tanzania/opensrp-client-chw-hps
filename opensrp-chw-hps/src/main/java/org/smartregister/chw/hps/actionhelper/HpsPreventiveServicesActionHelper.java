784a1494b6ab864d5022251419c042a934688fb7
cozej4 <ilakozejumanne@gmail.com>
2025-03-18 14:38:50 +0300

    :hammer: fixed saving of preventive_services HpsPreventiveServicesActionHelper

--- opensrp-chw-hps/src/main/java/org/smartregister/chw/hps/actionhelper/HpsPreventiveServicesActionHelper.java
+++ opensrp-chw-hps/src/main/java/org/smartregister/chw/hps/actionhelper/HpsPreventiveServicesActionHelper.java
@@ -1,15 +1,23 @@
 package org.smartregister.chw.hps.actionhelper;
 
+import static com.vijay.jsonwizard.constants.JsonFormConstants.FIELDS;
+import static com.vijay.jsonwizard.constants.JsonFormConstants.KEY;
+import static com.vijay.jsonwizard.constants.JsonFormConstants.VALUE;
+import static org.smartregister.client.utils.constants.JsonFormConstants.STEP1;
+
 import android.content.Context;
 
 import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.smartregister.chw.hps.domain.MemberObject;
 import org.smartregister.chw.hps.domain.VisitDetail;
 import org.smartregister.chw.hps.model.BaseHpsVisitAction;
 import org.smartregister.chw.hps.util.JsonFormUtils;
+import org.smartregister.client.utils.constants.JsonFormConstants;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -71,7 +79,28 @@ public class HpsPreventiveServicesActionHelper implements BaseHpsVisitAction.Hps
 
     @Override
     public String postProcess(String jsonPayload) {
-        return null;
+        try {
+            JSONObject jsonObject = new JSONObject(jsonPayload);
+            JSONArray fieldsArray = jsonObject.getJSONObject(STEP1).getJSONArray(FIELDS);
+
+            JSONObject preventiveServices = JsonFormUtils.getFieldJSONObject(fieldsArray, "preventive_services");
+            String value = preventiveServices.getString(VALUE);
+
+            List<String> valuesList = new ArrayList<>();
+            JSONArray valuesArray = new JSONArray(value);
+
+            for (int i = 0; i < valuesArray.length(); i++) {
+                JSONObject obj = valuesArray.getJSONObject(i);
+                valuesList.add(obj.getString(KEY));
+            }
+
+            preventiveServices.put(JsonFormConstants.VALUE, valuesList.toString());
+            return jsonObject.toString();
+        } catch (Exception e) {
+            Timber.e(e);
+            return null;
+        }
+
     }
 
     @Override
