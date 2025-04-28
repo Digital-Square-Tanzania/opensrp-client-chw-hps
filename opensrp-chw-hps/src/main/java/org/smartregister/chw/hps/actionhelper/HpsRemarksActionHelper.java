c91c950264622de723f1a12af6ac8223b2946a92
Faustina F. Honsiah <fhonsiah@gmail.com>
2025-04-28 13:06:39 +0300

    Change HPS library version

---
+++ opensrp-chw-hps/src/main/java/org/smartregister/chw/hps/actionhelper/HpsRemarksActionHelper.java
@@ -0,0 +1,96 @@
+package org.smartregister.chw.hps.actionhelper;
+
+import android.content.Context;
+
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.chw.hps.domain.MemberObject;
+import org.smartregister.chw.hps.domain.VisitDetail;
+import org.smartregister.chw.hps.model.BaseHpsVisitAction;
+import org.smartregister.chw.hps.util.JsonFormUtils;
+
+import java.util.List;
+import java.util.Map;
+
+import timber.log.Timber;
+
+public class HpsEducationOnBehaviouralChangeActionHelper implements BaseHpsVisitAction.HpsVisitActionHelper {
+    protected String jsonPayload;
+
+    protected String healthEducationProvided;
+
+    protected String baseEntityId;
+
+    protected Context context;
+
+    protected MemberObject memberObject;
+
+
+    public HpsEducationOnBehaviouralChangeActionHelper(Context context, MemberObject memberObject) {
+        this.context = context;
+        this.memberObject = memberObject;
+    }
+
+    @Override
+    public void onJsonFormLoaded(String jsonPayload, Context context, Map<String, List<VisitDetail>> map) {
+        this.jsonPayload = jsonPayload;
+    }
+
+    @Override
+    public String getPreProcessed() {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonPayload);
+            return jsonObject.toString();
+        } catch (JSONException e) {
+            Timber.e(e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onPayloadReceived(String jsonPayload) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonPayload);
+            healthEducationProvided = JsonFormUtils.getValue(jsonObject, "provision_of_preventive_services");
+        } catch (JSONException e) {
+            Timber.e(e);
+        }
+    }
+
+    @Override
+    public BaseHpsVisitAction.ScheduleStatus getPreProcessedStatus() {
+        return null;
+    }
+
+    @Override
+    public String getPreProcessedSubTitle() {
+        return null;
+    }
+
+    @Override
+    public String postProcess(String jsonPayload) {
+        return null;
+    }
+
+    @Override
+    public String evaluateSubTitle() {
+        return null;
+    }
+
+    @Override
+    public BaseHpsVisitAction.Status evaluateStatusOnPayload() {
+
+
+        if (StringUtils.isNotBlank(healthEducationProvided)) {
+            return BaseHpsVisitAction.Status.COMPLETED;
+        }
+        return BaseHpsVisitAction.Status.PENDING;
+    }
+
+    @Override
+    public void onPayloadReceived(BaseHpsVisitAction baseHpsVisitAction) {
+        Timber.v("onPayloadReceived");
+    }
+}
